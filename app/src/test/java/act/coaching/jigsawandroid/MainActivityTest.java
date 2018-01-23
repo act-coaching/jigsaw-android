6f12678ddd9bed5a3b576e56d8b2b3576eec9996
actmember <act.dev30@gamil.com>
2018-01-23 15:23:27 +0900

    app start story #1   _ testcode

--- app/src/test/java/act/coaching/jigsawandroid/MainActivityTest.java
+++ app/src/test/java/act/coaching/jigsawandroid/MainActivityTest.java
@@ -1,24 +1,27 @@
 package act.coaching.jigsawandroid;
+import android.widget.TextView;
+
+import junit.framework.Assert;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
-import static org.assertj.core.api.Java6Assertions.assertThat;
-import static org.robolectric.Shadows.shadowOf;
 @RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class)
 public class MainActivityTest {
-//    @Test
-//    public void ExitApp버튼을_클릭하면_앱을_종료한다() throws Exception {
-//        //Given
-//        MainActivity subject = Robolectric.setupActivity(MainActivity.class);
-//
-//        //When
-//        subject.findViewById(R.id.exitApp).performClick();
-//
-//        //Then
-//        assertThat(shadowOf(subject).isFinishing()).isEqualTo(true);
-//    }
+    @Test
+    public void ExitApp버튼을_클릭하면_앱을_종료한다() throws Exception {
+        //Given
+        MainActivity subject = Robolectric.setupActivity(MainActivity.class);
+
+        //When
+        //subject.findViewById(R.id.exitApp).performClick();
+
+        //Then
+        //assertThat(shadowOf(subject).isFinishing()).isEqualTo(true);
+        TextView tvAppTitle = (TextView)subject.findViewById(R.id.tvAppTitle);
+        Assert.assertEquals(tvAppTitle.getText(), "Jigsaw");
+    }
 }
