a2fdf6a9269f90abfa6caebe2ce92bc2a05a6f61
act-dev00 <act.dev00@gmail.com>
2018-01-24 15:24:32 +0900

    my page activity 업데이트

--- app/src/main/java/act/coaching/jigsawandroid/MyPageActivity.java
+++ app/src/main/java/act/coaching/jigsawandroid/MyPageActivity.java
@@ -2,20 +2,34 @@ package act.coaching.jigsawandroid;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+
+import act.coaching.jigsawandroid.service.ProjectService;
 
 /**
  * Created by actmember on 2018. 1. 23..
  */
 
-public class MyPageActivity extends AppCompatActivity{
+public class MyPageActivity extends AppCompatActivity {
+    ProjectService projectService;
+
+    void setprojectService(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    ProjectService getProjectService() {
+        if (projectService == null) {
+            return BaseApplication.retrofit.create(ProjectService.class);
+        }
+        return projectService;
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
 
-        if(getActionBar() != null) {
+        if (getActionBar() != null) {
             getActionBar().setTitle("my page");
         }
     }
